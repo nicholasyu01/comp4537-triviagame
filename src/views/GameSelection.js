7d2371899debc5c353a992865f8c24a3a945265e
IJYang <inseob.yang1@gmail.com>
2021-11-03 13:57:45 -0700

    Minor reformatting. Added comments.

--- src/views/GameSelection.js
+++ src/views/GameSelection.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import { Container, Button, Typography } from "@mui/material";
-import Quiz from "./quiz";
+import Quiz from "./Quiz";
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 
+// Component for selecting quiz. Has states for currently selected quiz and quiz data.
 export default function GameSelection(props) {
     const [gameSelected, setGameSelected] = useState('');
     const [gameData, setGameData] = useState([]);
     const history = useHistory();
     const { editMode } = props;
 
+    // Columns for characterizing and organizing quizzes.
     const columns = [
         {
             headerName: 'Game',
@@ -37,6 +39,8 @@ export default function GameSelection(props) {
             width: 140,
         },
     ];
+
+    // UNUSED
     const rows = [
         {
             id: 1,
@@ -60,6 +64,8 @@ export default function GameSelection(props) {
             date: '2020'
         },
     ];
+
+    // On load, sets quizzes in list onto the component's list.
     useEffect(() => {
         axios.get('http://localhost:5000/api/game')
             .then(games => {
@@ -70,6 +76,7 @@ export default function GameSelection(props) {
             })
     }, []);
 
+    // Returns the component.
     return (
         <div style={{ height: 600, width: '100%' }}>
             <DataGrid
@@ -87,10 +94,7 @@ export default function GameSelection(props) {
             {
                 editMode ?
                     <div>
-                        <Button
-                            onClick={() => history.push('/edit')}
-                            variant="contained"
-                        >
+                        <Button onClick={() => history.push('/edit')} variant="contained">
                             Create Game
                         </Button>
                         <Button
